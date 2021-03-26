0e96b36431d6379387818edd9c13e3c4f7953266
Tim Riscalas <timothy.riscalas@ocsbstudent.ca>
2021-03-26 17:23:19 -0400

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,11 @@
+from microbit import *
+
+#____ = _____
+# variable = value (expression)
+
+answer = 42
+hello_message = "Hi there!"
+
+display.scroll(hello_message)
+hello_message = "Yo!"
+display.scroll(hello_message)
